19a2e45a900056667e7645e24ae4b9500907f8dd
Phreework <phreework@gmail.com>
2018-05-20 13:33:38 +0800

    Some last refactoring
    make main program more readability.

---
+++ Java-OOP-Study/Java-OOPstudy/src/com/study/planeWar/Bullet.java
@@ -0,0 +1,34 @@
+package com.study.planeWar;
+
+public class Bullet extends FlyObject{
+
+
+	 /** 
+	  * 初始化数据 
+	  */
+    public Bullet(int x,int y){
+        this.x = x;
+        this.y = y;
+        this.sprite = PlaneGame.bullet;
+        setSpeed(3);
+    }
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		 y-=speed;
+	}
+
+	@Override
+	public boolean isOutScreen() {
+		// TODO Auto-generated method stub
+		  return y < -10;
+	}
+
+	@Override
+	public void step() {
+		// TODO Auto-generated method stub
+		fly();
+	}
+
+
+}
